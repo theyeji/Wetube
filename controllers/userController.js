39be9b2b09423eb60af40c6ee159cccff0d49f07
yeji <the-yeji@naver.com>
2021-01-18 23:14:42 +0900

    #2.25 More Controlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,10 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) => res.render("logout");
+export const logout = (req, res) => {
+  // TODO: process log out
+  res.redirect(routes.home);
+};
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
 export const changePassword = (req, res) => res.render("changePassword");
