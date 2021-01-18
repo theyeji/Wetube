fc532730b007529fd2c5bd46ca418f10723a33aa
yeji <the-yeji@naver.com>
2021-01-18 22:50:56 +0900

    #2.24 Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,11 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login");
+export const getLogin = (req, res) => res.render("login");
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout");
 export const userDetail = (req, res) => res.render("userDetail");
 export const editProfile = (req, res) => res.render("editProfile");
