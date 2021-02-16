c0cd80a05060ed0403001df86dc10a07a851dbd9
yeji <the-yeji@naver.com>
2021-02-16 16:37:09 +0900

    #7.1 Edit User Password

--- passport.js
+++ passport.js
@@ -21,6 +21,8 @@ passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
